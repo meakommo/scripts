4a085162052c58306d5536a8b3c062714936f0d9
River Kat <19921829+meakommo@users.noreply.github.com>
2021-12-03 13:11:41 +1000

    Update macsetup.sh

--- macsetup.sh
+++ macsetup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 declare -A clients
+clients[TEST]="ad.rgs.nz"
 clients[GEMS]="ruby.local"
 
 echo "Supported Client Codes \n
@@ -11,10 +13,13 @@ read clientCode
 echo "Please enter Domain admin account details"
 echo "Username"
 read username
-echo "Password"
-read passWord
 echo "Please enter the destination OU"
 read orgunit
 
 
-dsconfigad -a $HOSTNAME -u $username -ou $orgunit -domain client[$clientCode] -mobile enable -mobileconfirm enable -localhome enable -useuncpath enable -alldomains enable
+sudo dsconfigad -a $HOSTNAME -u $username -ou $orgunit -domain client[$clientCode] -mobile enable -mobileconfirm enable -localhome enable -useuncpath enable -alldomains enable -status
+
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers
+sleep 60
+echo "The computer will reboot in 60 seconds"
+shutdown -r now
