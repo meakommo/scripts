b2b28a9ae512e49f3878cf31c2d6fa4483735548
River Kat <19921829+meakommo@users.noreply.github.com>
2022-08-24 15:20:04 +1000

    Update macsetup.sh

--- macsetup.sh
+++ macsetup.sh
@@ -1,27 +1,25 @@
-#!/bin/bash
+#This script has been written in a way were it could be used to connect to multiple Active directory by adding them to the object
 
+#Domain list
 declare -a clients
-clients[1]="ad.rgs.nz"
-clients[2]="ruby.local"
+clients[1]="reiq.com.au"
 
+#default user group for the domain that all users are a member of
 declare -a clientallstaff
-clientallstaff[1]="domainusers@ad.rgs.nz"
-clientallstaff[2]="ad.allstaff@livinggems.com.au"
-
-declare -a netusrnme
-netusrnme[1]=kale.hembrow
-netusrnme[2]=ad.admin
-
+#I believe that this is correct for REIQ 
+clientallstaff[1]="users@reiq.com.au"
 
 
 echo "Supported Client Codes \n
-1 = TEST \n
-2 = GEMS \n"
-echo "Please enter the client code 1-9"
+1 = REIQ \n"
+echo "Please enter the client code"
 read clientCode
-echo "Enter the Password for a domain administrator"
+echo "Enter username with device enrollment right"
+read netusrnme
+echo "Password"
 read passwd
 localaccount= whoami
+computername= hostname
 echo $localaccount
 
 rc= ping -c 1 ${clients[$clientCode]}
@@ -32,10 +30,9 @@ if [[ $joindom == ${clients[$clientCode]} ]]; then
 	break
 else
 	if [[ $rc -eq 0 ]]; then
-		dsconfigad -force -computer adm-mac -domain ${clients[$clientCode]} -username ${netusrnme[$clientCode]} -p $passwd -mobile enable -mobileconfirm disable -localhome enable -useuncpath enable -alldomains enable -groups ${clientallstaff[$clientCode]}
+		dsconfigad -force -computer $computername -domain ${clients[$clientCode]} -username $netusrnme -p $passwd -mobile enable -mobileconfirm disable -localhome enable -useuncpath enable -alldomains enable -groups ${clientallstaff[$clientCode]}
 	else
 		echo "Unable to ping domain, check your connection to the domain and try again. \n EXITING!"
 		break
 	fi
 fi
-
